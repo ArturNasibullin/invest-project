bb126b0b32a3ce012e7300022070a9adbfb501f5
Artur Nasibullin <nasibullin.artur@gmail.com>
2020-12-25 09:52:37 +0500

    add calc, category.html + sass + js

--- app/js/main.js
+++ app/js/main.js
@@ -21,17 +21,51 @@ window.addEventListener('DOMContentLoaded', () => {
 	menuBtn('.left-sidebar__btn', '.left-sidebar__menu', '.left-sidebar__menu li');
 
 
+		//Power Range
+		var elem = document.querySelector('.calc__range');
+		var valueBox = document.getElementById('calc__summ-num');
+		var init = new Powerange(elem, {
+		hideRange     : true, 
+		min           : 100000, 
+		max           : 1000000, 
+		start         : 100000, 
+		step          : 100000, 
+		});
 
+		elem.addEventListener('change', () => {
+		valueBox.innerText = elem.value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+		});
 
+		//calc
+		let radio = document.querySelectorAll('input[name="programms"]'),
+			totalBox = document.querySelector('.calc__total strong'),
+			monthlyBox = document.querySelector('.calc__month strong'),
+			percent = 0.2,
+			money = 100000,
+			permonth = 9167,
+			month = 6;
 
+		radio.forEach((btn) => {
+			btn.addEventListener('change', function () {
+				percent = +btn.getAttribute('data-per');
+				month = +btn.getAttribute('data-month');
+				result = Math.round(percent / 12 * month * money);
+				total = result + money;
+				permonth = Math.round(total / 12);
+				monthlyBox.innerHTML = permonth.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+				totalBox.innerHTML = total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+			});
+		});
 
-
-
-
-
-
-
-
+		document.querySelector('.calc__range').addEventListener('change', function () {
+			money = +this.value;
+			result = Math.round(percent / 12 * month * money);
+			total = result + money;
+			permonth = Math.round(total / 12);
+			monthlyBox.innerHTML = permonth.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+			totalBox.innerHTML = total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+		});
+	
 
 
 
